6ecd3c30ec1e31e85e0b092094d2168bb893b354
Danino7 <omerda8277@gmail.com>
2023-04-01 15:43:52 +0300

    add e2e

---
+++ ScoreFile/e2e.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from ScoreFile.MainScores import url
+
+def test_scores_service():
+    my_driver = webdriver.Chrome()
+    my_driver.get(url)
+    score = my_driver.find_element(By.ID, "score")
+    score = int(score.text)
+    if 1 < score < 1000:
+        return True
+    else:
+        return False
+
+def main_function():
+   test_score = test_scores_service()
+   if test_score:
+       return 0
+   else:
+       return -1
+
